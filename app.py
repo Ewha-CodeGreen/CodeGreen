af35eeae1bbb3dfd6ecb36faf7057360e6f1c925
Ganghee-Lee-0522 <gh_5226@ewhain.net>
2024-10-23 20:05:57 +0900

    feat: add flask server

--- app.py
+++ app.py
@@ -1,4 +1,22 @@
 from flask import Flask, render_template
-import sys
 
-application = Flask(__name__)
+app = Flask(__name__)
+
+@app.route("/home")
+def hello():
+    return render_template("index.html")
+
+@app.route("/")
+def view_list():
+    return render_template("home.html")
+
+@app.route("/mypage")
+def view_review():
+    return render_template("mypage.html")
+
+@app.route("/productList")
+def reg_item():
+    return render_template("productList.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
